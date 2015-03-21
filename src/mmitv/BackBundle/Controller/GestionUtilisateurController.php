d2ae3236a4bc449282f4acf9d809ee95f91c87f8
Feyhna <ngu.prisca@gmail.com>
2015-03-21 20:22:18 +0100

    up21.03

--- src/mmitv/BackBundle/Controller/GestionUtilisateurController.php
+++ src/mmitv/BackBundle/Controller/GestionUtilisateurController.php
@@ -12,6 +12,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class GestionUtilisateurController extends Controller
 {
     public function afficheUserAction(){
-        return $this->render('mmitvBackBundle:GestionUtlisateur:gestionuser.html.twig');
+
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+
+        return $this->render('mmitvBackBundle:GestionUtilisateur:afficheuser.html.twig',
+            array(
+                'users' => $users
+            ));
     }
 }
